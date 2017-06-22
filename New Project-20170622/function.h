b4b9e5bb8ba02cdeca8932b8ccfa099a8cb21a3a
Ali <ali.mousavifar@gmail.com>
2017-06-22 10:04:16 -0700

    Created by Coding Ground Client

---
+++ New Project-20170622/function.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define FUNCTION_H
+#include <cstdio>
+
+void function (int i);
+
+#endif
